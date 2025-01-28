ff99bdc99d23a93847394093a68ec9a7225baefb
bdsm <36733200+sTePmImIMi@users.noreply.github.com>
2025-01-28 15:56:35 +0300

    создал карточку

---
+++ app/src/main/java/com/example/weatherbyjetpackcompose/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.weatherbyjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherbyjetpackcompose.screens.main_screen
+import com.example.weatherbyjetpackcompose.ui.theme.WeatherByJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherByJetpackComposeTheme {
+                main_screen()
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/weatherbyjetpackcompose/screens/main_screen.kt
@@ -0,0 +1,150 @@
+package com.example.weatherbyjetpackcompose.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.weatherbyjetpackcompose.R
+import com.example.weatherbyjetpackcompose.ui.theme.BlueLight
+
+@Preview(showBackground = true)
+@Composable
+fun main_screen() {
+    Image(
+        painter = painterResource(R.drawable.weather_bg),
+        contentDescription = "im1",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.5f),
+        contentScale = ContentScale.FillBounds,
+
+        )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    )
+    {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.5.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = BlueLight
+
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 0.dp
+            ),
+            shape = RoundedCornerShape(10.dp)
+        )
+        {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+
+                )
+            {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                )
+                {
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "20 Jun 2025 13:00",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White,
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/302.png",
+                        contentDescription = "im2",
+                        modifier = Modifier
+                            .size(35.dp)
+                            .padding(top = 3.dp, end = 8.dp)
+                    )
+                }
+                Text(
+                    text = "London",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "23°C",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                )
+                {
+                    IconButton(
+                        onClick =
+                        {
+
+                        },
+                    )
+                    {
+                        Icon(
+                            painter = painterResource(id = R.drawable.search),
+                            contentDescription = "im3",
+                            tint = Color.White,
+                        )
+                    }
+                    Text(
+                        text = "23°C/12°C",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+
+                    IconButton(
+                        onClick =
+                        {
+
+                        },
+                    )
+                    {
+                        Icon(
+                            painter = painterResource(id = R.drawable.sync),
+                            contentDescription = "im4",
+                            tint = Color.White,
+                        )
+                    }
+                }
+            }
+
+
+        }
+
+    }
+}
